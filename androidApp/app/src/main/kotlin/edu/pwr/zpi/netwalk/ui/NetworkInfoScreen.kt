243f6ec54406bff65982b576dda646bf7c9d4e48
Daniel Kosiorek <267600394+dkosiorekpwr@users.noreply.github.com>
2026-04-17 17:12:13 +0200

    added appropriate handling for TelephonyManager callback

--- androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/ui/NetworkInfoScreen.kt
+++ androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/ui/NetworkInfoScreen.kt
@@ -4,10 +4,12 @@ import android.telephony.TelephonyManager
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -15,12 +17,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import edu.pwr.zpi.netwalk.fetcher.LteNetworkInfo
 import edu.pwr.zpi.netwalk.fetcher.NetworkInfoData
 import edu.pwr.zpi.netwalk.fetcher.NetworkInfoFetcher
+import edu.pwr.zpi.netwalk.fetcher.NrNetworkInfo
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 
 @Composable
 fun NetworkInfoScreen(tm: TelephonyManager) {
-    var networkText by remember { mutableStateOf("Checking permissions...") }
+    var data by remember { mutableStateOf<NetworkInfoData?>(null) }
+    var errorText by remember {mutableStateOf<String?>(null)}
+    var hasPermission by remember { mutableStateOf(false) }
 
     val context = androidx.compose.ui.platform.LocalContext.current
 
@@ -29,31 +37,90 @@ fun NetworkInfoScreen(tm: TelephonyManager) {
         rememberLauncherForActivityResult(
             contract = ActivityResultContracts.RequestMultiplePermissions(),
         ) { results ->
-            val allGranted = NetworkInfoFetcher.getRequiredPermissions().all { results[it] == true }
-            networkText =
-                if (allGranted) {
-                    val info = NetworkInfoFetcher.fetchNetworkInfo(tm)
-                    formatNetworkInfo(info)
-                } else {
-                    "Permission denied"
-                }
+            hasPermission = NetworkInfoFetcher
+                .getRequiredPermissions()
+                .all { results[it] == true }
+
+            if (!hasPermission) {
+                errorText = "Permission denied"
+            }
         }
 
-    if (!NetworkInfoFetcher.hasRequiredPermissions(context)) {
-        SideEffect {
+    // LaunchedEffect potrzebny zamiast SideEffect - SideEffect wywołuje się po każdej rekompozycji
+    LaunchedEffect(hasPermission) {
+        if (!hasPermission) {
             permissionLauncher.launch(NetworkInfoFetcher.getRequiredPermissions())
+            return@LaunchedEffect
         }
-    } else {
-        val info = NetworkInfoFetcher.fetchNetworkInfo(tm)
-        networkText = formatNetworkInfo(info)
+
+        while (isActive) {
+
+            NetworkInfoFetcher.fetchNetworkInfo(tm, context) {
+                data = it
+            }
+
+            delay(5000)
+        }
     }
 
+
     Column(modifier = Modifier.padding(16.dp)) {
+
+        errorText?.let {
+            Text(text = it, color = Color.Red)
+        }
+
         Text(
-            text = networkText,
-            color = Color.White,
+            text = "Network: ${data?.networkType ?: "..."}",
+            color = Color.White
         )
+
+        Text("  5G NR  ", color = Color.White)
+
+        data?.nrCells?.forEach {
+            NrCellView(it)
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
+
+        Text(" LTE ", color = Color.White)
+
+        data?.lteCells?.forEach {
+            LteCellView(it)
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+
     }
 }
 
-private fun formatNetworkInfo(data: NetworkInfoData): String = "Network type: ${data.networkType} \nCell info: ${data.cellInfo}"
+@Composable
+fun LteCellView(cell: LteNetworkInfo, color: Color = Color.White) {
+    Text(
+        text = buildString {
+            append(if (cell.isServing) "LTE (Serving)\n" else "LTE (Neighbor)\n")
+            append("PCI: ${cell.pci}\n")
+            append("EARFCN: ${cell.earfcn}\n")
+            append("Bandwith: ${cell.bandwidth}\n")
+            append("RSRP: ${cell.rsrp} dBm\n")
+            append("RSRQ: ${cell.rsrq} dB\n")
+            append("SINR: ${cell.sinr} dB\n")
+        },
+        color = color
+    )
+}
+
+@Composable
+fun NrCellView(cell: NrNetworkInfo, color: Color = Color.White) {
+    Text(
+        text = buildString {
+            append(if (cell.isServing) "5G (Serving)\n" else "5G (Neighbor)\n")
+            append("PCI: ${cell.pci}\n")
+            append("NR-ARFCN: ${cell.nrarfcn}\n")
+            append("Band: ${cell.bands.joinToString()}\n")
+            append("RSRP: ${cell.ssRsrp}\n")
+            append("RSRQ: ${cell.ssRsrq}\n")
+            append("SINR: ${cell.ssSinr}\n")
+        },
+        color = color
+    )
+}
