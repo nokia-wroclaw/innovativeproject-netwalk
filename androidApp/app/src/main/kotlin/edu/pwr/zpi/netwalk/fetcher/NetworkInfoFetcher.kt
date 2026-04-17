87adcb37f7c907f6c02323f462b5d3970c4569e2
Daniel Kosiorek <267600394+dkosiorekpwr@users.noreply.github.com>
2026-04-17 16:55:04 +0200

    added updates for cellInfo and created classes for LTE and 5G networks

--- androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/fetcher/NetworkInfoFetcher.kt
+++ androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/fetcher/NetworkInfoFetcher.kt
@@ -3,19 +3,78 @@ package edu.pwr.zpi.netwalk.fetcher
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.telephony.CellIdentityNr
+import android.telephony.CellInfo
+import android.telephony.CellInfoLte
+import android.telephony.CellInfoNr
+import android.telephony.CellSignalStrengthNr
 import android.telephony.TelephonyManager
+import androidx.annotation.RequiresPermission
 import androidx.core.content.ContextCompat
+
+
+data class LteNetworkInfo(
+    val isServing: Boolean,
+    val pci: Int,
+    val earfcn: Int,
+    val bandwidth: Int,
+    val rsrp: Int,
+    val rsrq: Int,
+    val sinr: Int
+)
+
+data class NrNetworkInfo(
+    val isServing: Boolean,
+    val pci: Int,
+    val nrarfcn: Int,
+    val bands: List<Int>,
+    val ssRsrp: Int,
+    val ssRsrq: Int,
+    val ssSinr: Int
+)
 
 data class NetworkInfoData(
     val networkType: String,
-    val cellInfo: String,
+    val lteCells: List<LteNetworkInfo>,
+    val nrCells: List<NrNetworkInfo>
 )
+
+fun getLteInfo(cell: CellInfoLte): LteNetworkInfo {
+    val id = cell.cellIdentity
+    val signal = cell.cellSignalStrength
+
+    return LteNetworkInfo(
+        isServing = cell.isRegistered,
+        pci = id.pci,
+        earfcn = id.earfcn,
+        bandwidth = id.bandwidth,
+        rsrp = signal.rsrp,
+        rsrq = signal.rsrq,
+        sinr = signal.rssnr
+    )
+}
+
+fun getNrInfo(cell: CellInfoNr): NrNetworkInfo {
+    val id = cell.cellIdentity as CellIdentityNr
+    val signal = cell.cellSignalStrength as CellSignalStrengthNr
+
+    return NrNetworkInfo(
+        isServing = cell.isRegistered,
+        pci = id.pci,
+        nrarfcn = id.nrarfcn,
+        bands = id.bands.toList(),
+        ssRsrp = signal.ssRsrp,
+        ssRsrq = signal.ssRsrq,
+        ssSinr = signal.ssSinr
+    )
+}
+
 
 object NetworkInfoFetcher {
     private val REQUIRED_PERMISSIONS =
         arrayOf(
-            android.Manifest.permission.READ_PHONE_STATE,
-            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
         )
 
     fun hasRequiredPermissions(context: Context): Boolean =
@@ -25,14 +84,42 @@ object NetworkInfoFetcher {
 
     fun getRequiredPermissions(): Array<String> = REQUIRED_PERMISSIONS
 
-    fun fetchNetworkInfo(tm: TelephonyManager): NetworkInfoData {
-        val networkType =
-            when (tm.dataNetworkType) {
-                TelephonyManager.NETWORK_TYPE_NR -> "5G"
-                TelephonyManager.NETWORK_TYPE_LTE -> "LTE"
-                else -> "${tm.dataNetworkType}"
+    @RequiresPermission(value = "android.permission.ACCESS_FINE_LOCATION")
+    fun fetchNetworkInfo(tm: TelephonyManager, context: Context, onResult: (NetworkInfoData) -> Unit) {
+
+        tm.requestCellInfoUpdate(context.mainExecutor,
+            object: TelephonyManager.CellInfoCallback(){
+            @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
+            override fun onCellInfo(activeCellInfo: MutableList<CellInfo>){
+
+                val lteCells = mutableListOf<LteNetworkInfo>()
+                val nrCells = mutableListOf<NrNetworkInfo>()
+
+                for (cell in activeCellInfo) {
+                    when (cell) {
+                        is CellInfoLte -> lteCells.add(getLteInfo(cell))
+                        is CellInfoNr -> nrCells.add(getNrInfo(cell))
+                    }
+                }
+
+                val networkType = when (tm.dataNetworkType) {
+                    TelephonyManager.NETWORK_TYPE_NR -> "5G"
+                    TelephonyManager.NETWORK_TYPE_LTE -> "LTE"
+                    else -> "${tm.dataNetworkType}"
+                }
+
+                onResult(NetworkInfoData(networkType, lteCells, nrCells))
+
             }
-        val cellInfo = tm.allCellInfo?.toString() ?: "null"
-        return NetworkInfoData(networkType, cellInfo)
+
+                override fun onError(errorCode: Int, detail: Throwable?) {
+                    onResult(NetworkInfoData("Cell info error ${errorCode}:" +
+                            " ${detail?.message}",
+                        lteCells = emptyList(),
+                        nrCells = emptyList()))
+                }
+
+        }
+        )
     }
 }
