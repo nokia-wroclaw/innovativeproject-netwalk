3b2e65d748a8106a6f3204d5696a21a77d250195
Dolomirr <dolomirr@gmail.com>
2026-04-20 21:11:41 +0200

    added middleware support for FastAPI application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import Base, engine
 from app.routes import router
@@ -6,5 +7,12 @@ from app.routes import router
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="NetWalk API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=("*"),
+    allow_credentials=True,
+    allow_methods=("*"),
+    allow_headers=("*"),
+)
 
 app.include_router(router)
