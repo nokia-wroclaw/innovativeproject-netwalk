dc496af2388756a39b06fa989d6c5a719dcdd707
Daniel Kosiorek <267600394+dkosiorekpwr@users.noreply.github.com>
2026-04-26 22:23:55 +0200

    implemented parallel network and location fetch     - implemented parallel data fetching for NetworkInfo and Location     - added explicit permission handling and UI state updates for location coordinates

--- androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/ui/NetworkViewModel.kt
+++ androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/ui/NetworkViewModel.kt
@@ -1,5 +1,6 @@
 package edu.pwr.zpi.netwalk.ui
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.telephony.TelephonyManager
 import androidx.compose.runtime.getValue
@@ -7,18 +8,25 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import edu.pwr.zpi.netwalk.fetcher.MeasurementRequest
 import edu.pwr.zpi.netwalk.fetcher.NetworkInfoData
 import edu.pwr.zpi.netwalk.fetcher.NetworkInfoFetcher
 import edu.pwr.zpi.netwalk.fetcher.toMeasurementsRequest
+import edu.pwr.zpi.netwalk.location.getCurrentLocation
 import edu.pwr.zpi.netwalk.network.NetworkClient
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.time.LocalTime
+import kotlin.Double
+import kotlin.Pair
 
 class NetworkViewModel : ViewModel() {
-    var uiState by mutableStateOf<NetworkInfoData?>(null)
+    var uiStateNetwork by mutableStateOf<NetworkInfoData?>(null)
+        private set
+    var uiStateLocation by mutableStateOf<Pair<Double?, Double?>>(null to null)
         private set
     var lastStatus by mutableStateOf("Waiting for first fetch...")
         private set
@@ -27,6 +35,7 @@ class NetworkViewModel : ViewModel() {
     private val client = NetworkClient("http://10.0.2.2:8000")
     private var collectionJob: Job? = null
 
+    @SuppressLint("MissingPermission")
     fun startCollection(
         tm: TelephonyManager,
         context: Context,
@@ -36,21 +45,30 @@ class NetworkViewModel : ViewModel() {
 
         collectionJob = viewModelScope.launch {
             while (isActive) {
-                NetworkInfoFetcher.fetchNetworkInfoSafe(tm, context) { fetchedData ->
+                if (NetworkInfoFetcher.hasRequiredPermissions(context)) {
+                    val networkAsyncData = async { NetworkInfoFetcher.fetchNetworkInfo(tm, context) }
+                    val locationAsyncData = async { getCurrentLocation(context) }
 
-                    // tu można dodać processing etc.
-                    uiState = fetchedData
-                    sendToServer(fetchedData)
+                    val networkData = networkAsyncData.await()
+                    val locationData = locationAsyncData.await()
+
+                    uiStateNetwork = networkData
+                    uiStateLocation = locationData
+
+                    val (lat, lon) = locationData
+                    val request = networkData.toMeasurementsRequest(lat, lon)
+                    sendToServer(request)
+                } else {
+                    lastStatus = "Permissions missing - cannot fetch data."
                 }
+
                 delay(5000)
             }
         }
     }
 
-    private fun sendToServer(data: NetworkInfoData) {
+    private fun sendToServer(request: MeasurementRequest) {
         viewModelScope.launch {
-            val request = data.toMeasurementsRequest()
-
             client
                 .sendFullUpdate(request)
                 .onSuccess {
