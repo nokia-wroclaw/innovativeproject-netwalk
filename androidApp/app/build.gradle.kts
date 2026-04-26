4944432205a5d5361d7dae8fbd18e389e5a5b959
Daniel Kosiorek <267600394+dkosiorekpwr@users.noreply.github.com>
2026-04-26 20:54:08 +0200

    updated dependencies for location fetching

--- androidApp/app/build.gradle.kts
+++ androidApp/app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.play.services.location)
     debugImplementation(libs.androidx.ui.tooling)
 }
 
