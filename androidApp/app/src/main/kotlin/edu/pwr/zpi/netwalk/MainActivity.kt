7e90c0fbd8546ce75ffa201ce36cb26b7d32c020
Daniel Kosiorek <267600394+dkosiorekpwr@users.noreply.github.com>
2026-04-26 21:41:53 +0200

    added LocationClient

--- androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/MainActivity.kt
+++ androidApp/app/src/main/kotlin/edu/pwr/zpi/netwalk/MainActivity.kt
@@ -5,15 +5,19 @@ import android.telephony.TelephonyManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import edu.pwr.zpi.netwalk.ui.NetworkInfoScreen
 
 class MainActivity : ComponentActivity() {
     lateinit var tm: TelephonyManager
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         tm = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         setContent {
             MaterialTheme {
