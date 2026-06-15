46562057fa61fe3649ff263c3d6ee8835ff3db63
Dolomirr <dolomirr@gmail.com>
2026-06-16 00:18:37 +0200

    re-applied auth verification to batch post route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -292,6 +292,7 @@ def get_device_sessions(
 async def create_measurements_batch(
     request: Request,
     db: DbSession,
+    _: None = Depends(verify_basic_auth),
 ):
     raw_body = await request.body()
 
